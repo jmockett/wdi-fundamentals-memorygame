1dec55ef69e9755b3a4fde5da78b46d2985b9126
Julie Mockett <juliemockett@Julies-MacBook-Air.local>
2018-01-04 17:53:34 -0800

    modified main.js

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,7 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ["queen","queen","king","king"];
+cardsInPlay[];
+var cardOne[0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
